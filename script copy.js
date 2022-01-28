c99365c760cc2e14236cdd6e45f7a03760017388
aerebho13 <97203978+aerebho13@users.noreply.github.com>
2022-01-28 15:48:02 -0800

    Add files via upload

---
+++ script copy.js
@@ -0,0 +1,36 @@
+console.log("Script");
+let client;//1. declariton
+client="Aaron";//2. assignation
+let age="28";
+const URL="http://127.0.0.1";
+age=55;
+
+
+console.log(URL);
+client="Maria";
+
+client="Aaron";
+
+console.log("client");//client
+//console.log("Please, " + client + " visit the site " + URL);
+
+let children="9";
+let partner="Rihanna";
+let geoLocation="San Diego";
+let Job="Software Developer";
+let ma="marriage";
+let eu="Europe";
+let js="Jamaican-style";
+let bs="Barcelona, Spain";
+let cod="couple of decades";
+let re="retire";
+let bc="beach city";
+let sos="south of Spain";
+let mar="Marbella";
+let par="partying";
+let surf="surfing";
+let eat="eating lots of fish";
+let ttl="travel to lands";
+document.write("I " + client + " will be a " + Job + " in " + geoLocation + " and married to " + partner + " with " + children + " kids. After my " + ma + ", I plan on moving back to " + eu + " and opening a " + js + " restaurant in " + bs + " run by my family and me. Then after doing that for a " + cod + " or so, I will " + re + "and move to a " + bc + "in the " + sos + " called " + mar + ". There I will live out the rest of my years with my wife " + par + ", " + surf + ", " + eat + ", and occasionally leaving the country to " + ttl + "Ive never been before."); 
+
+
